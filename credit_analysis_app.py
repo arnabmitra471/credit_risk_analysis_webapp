9252999d846fdd07a6c78d7efc62adb796f8b4a4
Arnab Mitra <arnabmitra471@gmail.com>
2026-07-17 21:55:24 +0530

    Initial commit

---
+++ credit_analysis_app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.set_page_config(page_title="Credit Risk intelligence dashboard",
+                   page_icon="🔐",
+                   initial_sidebar_state="expanded",layout="wide")
+
+st.title("Credit risk analysis web app")
+st.subheader("Identifying loan defaulters")
