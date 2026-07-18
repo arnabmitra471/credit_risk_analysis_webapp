440b3b7c74bc0f9be7980badbadf8e380391dba0
Arnab Mitra <arnabmitra471@gmail.com>
2026-07-18 13:35:08 +0530

    Add credit risk analysis dashboard with model loading functionality

--- credit_analysis_app.py
+++ credit_analysis_app.py
@@ -1,8 +1,37 @@
 import streamlit as st
-
+import joblib
 st.set_page_config(page_title="Credit Risk intelligence dashboard",
                    page_icon="🔐",
                    initial_sidebar_state="expanded",layout="wide")
 
-st.title("Credit risk analysis web app")
-st.subheader("Identifying loan defaulters")
+st.markdown("""
+            <style>
+            .main-title{
+                font-size: 2.5rem;
+                font-weight: bold;
+                color: #1E3A8A;
+            
+            }
+            .subtitle { font-size: 1.1rem; 
+            color: #4B5563; 
+            margin-bottom: 25px; 
+            }
+            div[data-testid="stMetricValue"] { font-size: 2rem; font-weight: 700; color: #1E3A8A; }
+            </style>
+            """,unsafe_allow_html=True)
+
+st.markdown('<div class="main-title">Credit risk intelligence and ensemble analytics dashboard</div>',unsafe_allow_html=True)
+st.markdown('<div class="subtitle">An interactive deployment showcasing structural feature transformations, operational evaluation cutoffs, and transparent Random Forest tree tracking.</div>', unsafe_allow_html=True)
+
+# Loading the resources
+@st.cache_resource
+def load_pipeline_assets():
+    try:
+        rf_model = joblib.load("final_rf_model.pkl")
+        preprocessor = joblib.load("preprocessor_pipeline.pkl")
+        feature_selector = joblib.load("feature_selector.pkl")
+        return rf_model,preprocessor,feature_selector
+    except:
+        return None,None,None
+final_rf,preprocessor,selector = load_pipeline_assets()
+
