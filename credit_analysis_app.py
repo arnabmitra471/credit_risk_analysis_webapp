24f8ffa453c2dc6334de7cced9d165decb2d6b7b
Arnab Mitra <arnabmitra471@gmail.com>
2026-07-22 13:59:43 +0530

    Enhance model loading functionality with file upload support for random forest model, preprocessor, and feature selector

--- credit_analysis_app.py
+++ credit_analysis_app.py
@@ -23,17 +23,25 @@ st.markdown("""
 st.markdown('<div class="main-title">Credit risk intelligence and ensemble analytics dashboard</div>',unsafe_allow_html=True)
 st.markdown('<div class="subtitle">An interactive deployment showcasing structural feature transformations, operational evaluation cutoffs, and transparent Random Forest tree tracking.</div>', unsafe_allow_html=True)
 
+uploaded_model = st.sidebar.file_uploader("Upload random forest model(.pkl)",type=[".pkl"])
+uploaded_preprocessor = st.sidebar.file_uploader("Upload Preprocessor(.pkl)",type=[".pkl"])
+uploaded_feat_selector = st.sidebar.file_uploader("Upload Feature Selector(.pkl)",type=[".pkl"])
 # Loading the resources
 @st.cache_resource
-def load_pipeline_assets():
+def load_updated_pipeline(model_file,prep_file,sel_file):
     try:
-        rf_model = joblib.load("final_rf_model.pkl")
-        preprocessor = joblib.load("preprocessor_pipeline.pkl")
-        feature_selector = joblib.load("feature_selector.pkl")
+        rf_model = joblib.load(model_file)
+        preprocessor = joblib.load(prep_file)
+        feature_selector = joblib.load(sel_file)
         return rf_model,preprocessor,feature_selector
-    except:
+    except Exception as e:
         return None,None,None
-final_rf,preprocessor,selector = load_pipeline_assets()
+if uploaded_model and uploaded_feat_selector and uploaded_feat_selector:
+    final_rf,preprocessor,selector = load_updated_pipeline(uploaded_model,uploaded_preprocessor,uploaded_feat_selector)
+    st.sidebar.success("All three pipeline assets loaded into memory")
+else:
+    final_rf,preprocessor,selector = None,None,None
+    st.sidebar.info("Please upload all the 3 .pkl files to unlock the full experience")
 
 st.sidebar.header("Control Panel")
 app_mode = st.sidebar.radio("Navigate Workspace",["Single applicant","Random forest structural inspector"])
